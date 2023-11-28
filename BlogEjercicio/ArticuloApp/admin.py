39f173e76fb8c7726783acda23568fc82432483a
Manuel Encalada <manualencalada@gmail.com>
2023-11-28 15:54:47 -0300

    Modelos mejorados

--- BlogEjercicio/ArticuloApp/admin.py
+++ BlogEjercicio/ArticuloApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import post, category, PostCategory, comment
 # Register your models here.
+
+admin.site.register(post)
+admin.site.register(category)
+admin.site.register(PostCategory)
+admin.site.register(comment)
+
--- BlogEjercicio/ArticuloApp/models.py
+++ BlogEjercicio/ArticuloApp/models.py
@@ -17,9 +17,48 @@ class post(models.Model):
     content = models.TextField(max_length= 100, null=False)
     create_at = models.DateField(auto_created=True)
     update_at = models.DateField(auto_now_add= True)
+    category = models.ManyToManyField("PostCategory")
     status = models.IntegerField(choices=STATUS_CHOICES , default= 1)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False, blank=False, unique=True)
     image = models.ImageField(upload_to="image", null = True, blank = True)
 
     def __str__(self):
         return self.title
+
+COLOR_CHOICES = (
+    ('1', 'celeste'),
+    ('2', 'beige'),
+    ('3', 'salmon')
+)
+
+NAME_CHOICES = (
+    ('1', 'nacional'),
+    ('2', 'provincial'),
+    ('3', 'municipal')
+)
+
+class category (models.Model):
+    name= models.TextField(choices= NAME_CHOICES, default = 'nacional')
+    description = models.TextField()
+    slug = models.SlugField(null=False, blank=False, unique=True)
+    color = models.CharField(max_length=10, choices= COLOR_CHOICES, default= 'celeste')
+
+    def __str__(self):
+        return self.name
+
+class PostCategory (models.Model):
+    posts = models.ForeignKey(post, on_delete= models.CASCADE)
+    categories = models.ForeignKey(category, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.post
+
+class comment (models.Model):
+    author = models.CharField(max_length= 20, null = False)
+    content = models.TextField(null = False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.author
+
